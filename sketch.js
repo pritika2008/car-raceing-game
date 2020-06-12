dd21feddeb6c4db88271aedece8b5f6f14a4ff3d
pritika2008 <64711031+pritika2008@users.noreply.github.com>
2020-06-11 20:55:52 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var hypnoticBall, database, position;
+
+
+function setup(){
+    database = firebase.database();
+    console.log(database);
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    ball.x = ball.x + x;
+    ball.y = ball.y + y;
+}
